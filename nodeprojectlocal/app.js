ed2ed06fb79bf568e4a4f096cf9f88fcfb341007
vsplash <vsplash@system-b108.vsplash.local>
2017-08-01 16:49:20 +0530

    added express with npm package manager

--- nodeprojectlocal/app.js
+++ nodeprojectlocal/app.js
@@ -1,1 +1,6 @@
-//asdf
+var express = require('express');
+console.log("express is created as "+ express);
+var expressInstance = express()
+expressInstance.listen(8080, function(err) {
+    console.log("the server is listening at 8080");
+});
