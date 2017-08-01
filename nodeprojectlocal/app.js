23639d2a08a67c3ce5009bb7c7d4b0efb6360760
vsplash <vsplash@system-b108.vsplash.local>
2017-08-01 17:51:21 +0530

    added bootstrap template

--- nodeprojectlocal/app.js
+++ nodeprojectlocal/app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 console.log("express is created as "+ express);
 var expressInstance = express()
+
 expressInstance.listen(8000, function(err) {
-    //you can kill all node established servers by typing "killall -9 node" in terminal
+    //you can kill all node established servers by typing "killall -9 node" in terminal, this is fix for "EADDRINUSE" error
     console.log("the server is listening at 8000");
 });
+
+
+//this is to set the static directory, request check for files in static directory first, if not found then looks at other path specified in expressInstance.get
+expressInstance.use(express.static("public/bootstrap_templates/nice_to_meet_you"));
 
 expressInstance.get("/", function(req, response) {
   
