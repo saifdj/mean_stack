8f13de8eac9a0f080122827b30a9711bbb0315fe
saif <saifuddin.ece@gmail.com>
2017-08-02 16:49:11 +0530

    added ejs templating package

--- nodeprojectlocal/app.js
+++ nodeprojectlocal/app.js
@@ -12,7 +12,11 @@ expressInstance.listen(8000, function(err) {
 //this is to set the static directory, request check for files in static directory first, if not found then looks at other path specified in expressInstance.get
 expressInstance.use(express.static("public/bootstrap_templates/nice_to_meet_you"));
 //now we added one more static dir, when request comes in express look in nice_to_meet_you, if not found then look into views 
-expressInstance.use(express.static("src/views"));
+// expressInstance.use(express.static("src/views"));
+
+expressInstance.set("views", "./src/views"); //same as app.use
+expressInstance.set("view engine", "ejs"); //view engine for our app is ejs, it is light weight node templating engine
+
 
 //use "bower upgrade" to update bower components
 expressInstance.use(express.static("bower_components")); //now remove references to core bootstrap & jquery in index.html ( vendor/bootstrap/js/bootstrap.min.js should be removed and pointed to bower_components/bootstrap/XXXXXXX appropriate path, already done in index.html, search for core and see the changes) 
@@ -24,5 +28,8 @@ expressInstance.get("/", function(req, response) {
     //you can make a hit to the server which we exstablished at port 8000, opening http://127.0.0.1:8000/ in browser, this call back will be executed and response.send string will be visible in browser
 
     console.log("got a request from client browser");
-    response.send("Sending this string as response to client/browsers request");
+    // response.send("Sending this string as response to client/browsers request");
+    response.render("index", {"prarms_for_ejs": ["saif", "mobile app dev", "mean", "node"]});
 });
+
+
