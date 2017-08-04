854337ab279c30ada148cb1a0ac63e80edf81e77
saif <saifuddin.ece@gmail.com>
2017-08-04 17:56:39 +0530

    used express.router for routing

--- nodeprojectlocal/app.js
+++ nodeprojectlocal/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 // console.log("express is created as "+ express);
 var app = express();
 
+//used to route, same as app.get
+var eventRouter = express.Router();
 
 //this is to set the static directory, request check for files in static directory first, if not found then looks at other path specified in app.get
 app.use(express.static("public/bootstrap_templates/nice_to_meet_you/"));///bootstrap_templates/nice_to_meet_you
@@ -21,21 +23,42 @@ app.set("view engine", "ejs"); //view engine for our app is ejs, it is light wei
 
 
 
-app.get("/", function(req, response) {
-  
+
+eventRouter.route("/").get(function (req, response) {
+    response.send("Events");
+}); //it can also be a .post
+
+
+eventRouter.route("/event").get(function (req, response) {
+    response.send("Single Event");
+}); //it can also be a .post, executes when hit has /events/event
+
+app.use("/Events", eventRouter) //uses eventRouter when url has /events
+
+
+
+app.get("/", function (req, response) {
+
     //you can make a hit to the server which we exstablished at port 8000, opening http://127.0.0.1:8000/ in browser, this call back will be executed and response.send string will be visible in browser
 
     console.log("got a request from client browser");
     // response.send("Sending this string as response to client/browsers request");
     response.render("index", {
         "prarms_for_ejs": ["saif", "mobile app dev", "mean", "node"],
-        "nav_elements" : ["Services","Portfolio", "About", "Team", "Contact"]
+        "nav_elements": [{
+            "text": "Services", "link": "services"}, 
+            { "text": "Portfolio", "link": "portfolio" }, 
+            { "text": "About", "link": "about" }, 
+            { "text": "Team", "link": "team" }, 
+            { "text": "Contact", "link": "contact" },
+            { "text": "Events", "link": "" }
+        ]
 
     });
 });
 
 
-app.listen(8000, function(err) {
+app.listen(8000, function (err) {
     //you can kill all node established servers by typing "killall -9 node" in terminal, this is fix for "EADDRINUSE" error
     console.log("the server is listening at 8000");
 });
